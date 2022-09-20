8515c9ecbfb1139e9002bb223cb5a877273ea321
Samuel-col <68486038+Samuel-col@users.noreply.github.com>
2022-09-19 22:21:22 -0500

    Add files via upload

--- test.jl
+++ test.jl
@@ -13,6 +13,13 @@ CSV.read("data/SleepStudyData.csv",DataFrame)
 df = dropmissing(df)
 
 mod = Model(df,(true,false,true,true,true),:Tired,:Enough,:Hours,:PhoneReach,:PhoneTime,:Breakfast)
+# El primer argumento es el dataframe.
+# El segundo argumento es una tupla cuya i-ésima entrada es true si la i-ésima Variable
+#  explicativa es un factor y false si no.
+# EL tercer argumento es el nombre de la variable dependiente
+# Los siguientes argumentos son las variables explicativas. Deben ser tantas como entradas
+#  tenga la tupla del segundo argumento.
+
 
 display(mod.X)
 
