fcb35efcc674ce82838945dc19866dd8b38800fc
Samuel-col <68486038+Samuel-col@users.noreply.github.com>
2022-09-20 21:20:40 -0500

    Add files via upload

--- src/base_functions.jl
+++ src/base_functions.jl
@@ -45,3 +45,45 @@ function rank(V::Array{<:Real,1})
     return 1
 end
 
+function removeChar(c::Char,s::String)::String
+    l = length(s)
+    ind = findall(c,s)
+    newInd = symdiff(1:l,ind)
+    return s[newInd]
+end
+
+function stringBreak(s::String,indices::Vector{Int})::Vector{String}
+    push!(indices,0)
+    push!(indices,length(s)+1)
+    indices = sort(indices)
+    return [s[(indices[i-1]+1):(indices[i]-1)] for i in 2:length(indices)]
+end
+
+function readFormula(s::String)
+    specialChar = ['~','+','*',':']
+    
+    s = removeChar(' ',s)
+    l = length(s)
+    equal_position = findfirst('~',s)
+    
+    y_str = SubString(s,1,equal_position-1)
+    x_str = SubString(s,equal_position+1,l)
+    
+    #specialChar_pos = vcat(findall.(specialChar,x_str)...)
+    plus_pos = findall('+',x_str)
+    regressors_str = stringBreak(string(x_str),plus_pos)
+
+    for i in 1:length(regressors_str)
+        fctr = regressors_str[i]
+        if occursin('*',fctr)
+            interaction = replace(fctr,'*' => ':')
+            regressors_str[i] = interaction
+            actors = stringBreak(fctr,[findfirst('*',fctr)])
+            push!(regressors_str,actors...)
+        end
+    end
+    
+    return y_str,sort(regressors_str,by = length)
+end
+
+
